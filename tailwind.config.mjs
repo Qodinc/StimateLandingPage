8ff2dffb2b7106ee184c0a87a774854bfca804df
Molar <grubenmj@gmail.com>
2024-09-03 19:51:45 -0600

    "The fonts sizes were updated"

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				"primary": "#2F27CE"
-			}
-		},
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    fontSize: {
+		footerM: "0.5rem",
+		baseM: "0.8125rem",
+		base: "1rem",
+		zoom: "1.25rem",
+		lg: "1.5rem",
+		xl: "2rem",
+		"2xl": "2.25rem",
+		"3xl": "3rem",
+		"4xl": "4rem",
 	},
-	plugins: [],
-}
+  },
+  plugins: [],
+};
